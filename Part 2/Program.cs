8b2ae83dbf379f23c9803fbdc35e1c8f143d6d99
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-05-12 15:29:50 +0200

    Parsing  ( rzutowania)

---
+++ Part 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine();
+            int yearOfBirth;
+            if (int.TryParse(userInput, out yearOfBirth))
+            {
+                int age = DateTime.Now.Year - yearOfBirth;
+                Console.WriteLine($"User ages is : {age} ");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect Value");
+            }
+            byte byteValue = 100;
+            int intValue = byteValue;
+            byte byteValue2 = (byte)intValue;
+            double doubleValue = 3.5;
+            int intValue2 = (int)doubleValue;  //= 3 .
+          
+
+
+
+        }
+    }
+}
