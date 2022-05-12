f4816bf8269840159d622d98fd54c56cd5d5639c
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-05-12 16:05:47 +0200

    Task1
    Zadanie:
    
    Utwórz aplikację konsolową, która będzie wczytywać wartości liczbowe od użytkownika, dopóki nie poda on wartości 0.
    
    Podczas czytania, aplikacja powinna:
    
    obliczyć sumę wszystkich elementów
    
    znaleźć największy element
    
    Po wczytaniu wartości 0, te dwie wartości powinny zostać wypisane w konsoli.

---
+++ Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = 0;
+            int enteredNumber = 1;
+            int sum = 0;
+            Console.Write("Enter a number: ");
+            while (enteredNumber != 0)
+            {
+                string stringToParse = Console.ReadLine();
+                if (int.TryParse(stringToParse, out enteredNumber))
+                {
+                    Console.Write("Enter a number: ");
+                    maxNumber = Math.Max(maxNumber, enteredNumber);
+                    sum += enteredNumber;
+                }
+                else
+                {
+                    Console.Write("Wrong data type!\n Try again :");
+                    enteredNumber = 1;
+                }
+            }
+
+            Console.WriteLine("Biggest number u write: " + maxNumber);
+            Console.WriteLine("All number sum: " + sum);
+
+        }
+    }
+}
