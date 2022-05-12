8ddcdc3ab2f89f8c29cdfaf921dc0db8b1942e44
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-05-12 14:55:52 +0200

    Comments and class description
    Comments

---
+++ BacisPart1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BacisPart1
+{
+    #region Main Function
+
+    internal class Program
+    {
+
+        /// <summary>
+        ///  main method description Cos tam
+        /// </summary>
+        /// <param name="args">args description</param>
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+            // .........    (logiczna) ? True : False;
+            int graterNumber = x > y ? x : y;
+
+        }
+    }
+    #endregion 
+}
