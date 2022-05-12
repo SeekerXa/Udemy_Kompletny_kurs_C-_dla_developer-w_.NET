62ca15c25c4daccaf4c0cfc947c84614011dfec0
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-05-12 16:26:44 +0200

    Enum Type

---
+++ Enum type/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your gender 1 = men, 2 = female:");
+            string answer = Console.ReadLine();
+           
+            Gender userGender = (Gender)Enum.Parse(typeof(Gender),answer);
+           
+            if(userGender == Gender.Male)
+            {
+                Console.WriteLine("only woman allow");
+            }
+            else
+            {
+                Console.WriteLine("Hi");
+            }
+
+
+        }
+    }
+}
