16299164db78d49f494cb2e0f0ce1358c2dfb1a7
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-05-12 14:39:04 +0200

    System.Text
    StringBuilder class

--- NewProject/Program.cs
+++ NewProject/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+
+class Program
+{
+
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+        string someText = "some text";
+        char jchar = 'j';
+        char jCharUnicode = '\u006A';
+
+        bool isUserReady = false;
+
+        DateTime now = DateTime.Now;
+        DateTime dayOfBirth = new DateTime(1990, 06, 02);
+
+        float floatNumber = 1.5f;
+        decimal decimalNumber = 1.5M;
+        double doubleNumber = 1.5;
+
+        string text = "he said \"hi\"";
+
+        string fontsFolder = @"c:\windows\fonsts";
+
+        string concatenated = string.Concat(text, fontsFolder, "me");
+        string concatenated2 = text + "too";
+        string interpolated = $"{text}";
+
+        StringBuilder sb = new StringBuilder("this is first:");
+        sb.Append("is");
+        sb.Append("a");
+        sb.Append("long");
+        sb.Append("text");
+        Console.WriteLine(sb);
+
+        String result = sb.ToString();
+        Console.WriteLine(result);
+    }
+
+}
+
