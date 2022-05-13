b94aee52f53c8863067816ce4b0805ace45aa7a9
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-05-13 14:23:28 +0200

    Class01
    Introduction,Metods, Constructor, Get;Set; Static

---
+++ ClassTut/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTut
+{
+    internal class Person
+    {
+        public static int Count = 0;
+        public string FirstName;
+        public string LastName;
+        DateTime dateOfBirth;
+        string contactNumber;
+
+        public string ContactNumber
+        {
+            get { return contactNumber; }
+            set
+            {
+                if (value.Length > 9 && value.Length < 9)
+                {
+                    Console.WriteLine("Wrong number");
+                }
+                else contactNumber = value;
+            }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get { return dateOfBirth; }
+            set
+            {
+                if (value > DateTime.Now)
+                {
+                    Console.WriteLine("Invaild date if birth");
+                }
+                else dateOfBirth = value;
+            }
+        }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Count++;
+        }
+
+        public Person(string firstName, string lastName, DateTime date) : this(firstName, lastName)
+        {
+            DateOfBirth = date;
+        }
+
+    
+
+        public DateTime GetDateOfBirth() => dateOfBirth;
+
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi, I'm {FirstName}, {GetDateOfBirth()}");
+        }
+
+
+
+    }
+}
---
+++ ClassTut/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person bill = new Person("Bill", "Whick", new DateTime(1999, 1, 1));
+
+            bill.SayHi();
+            bill.ContactNumber = "123345567";
+
+            Console.WriteLine(Person.Count);
+
+
+        }
+
+    }
+}
