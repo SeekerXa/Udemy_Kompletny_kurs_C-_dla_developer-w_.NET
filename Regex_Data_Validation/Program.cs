f3fed38a3e75a317f663d4ba2c16fa6d590cbf48
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-05-13 13:07:57 +0200

    Regex String Validation

---
+++ Regex_Data_Validation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace Regex_Data_Validation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Regex regex = new Regex(@"^[a-z0-9]+\w.?[a-z0-9]+@[a-z0-9]+\.[a-z]{2,3}$");
+
+            string email = "test_t2@gmail.com";
+            
+            Console.WriteLine(regex.IsMatch(email));
+        }
+    }
+}
