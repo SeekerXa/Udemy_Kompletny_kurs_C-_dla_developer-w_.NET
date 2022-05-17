4c0127501421ae8452528d0a3f642c94c4c8930c
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-05-17 23:39:08 +0200

    list and its properties

--- collectors/Program.cs
+++ collectors/Program.cs
@@ -8,16 +8,46 @@ namespace collectors
 {
     internal class Program
     {
+        static void DispayElements(List<int> list)
+        {
+            Console.WriteLine("** List **");
+            foreach (int i in list)
+            {
+                Console.Write(i + ", ");
+            }
+            Console.WriteLine();
+        }
+
+
         static void Main(string[] args)
         {
             //Dictionary , HashTable       klucz, wartość
             //List                         dodawanie usuwanie filtrowanie sortowanie grupowanie
             //queue , Stack                First in first Out,    First in last Out
 
-            List<int> intList= new List<int>() { 6, 1, 20 };
-            intList.Add(6); 
+            List<int> intList = new List<int>() { 6, 1, 20, 3, 45, 60, 100, 2, 6, 1, 20, 3, 45, 60, 100, 2 };
+
+            Console.Write("New Element: ");
+            intList.Add(int.Parse(Console.ReadLine()));      //dodawanie z parsowaniem
+
+            intList.RemoveAt(0); //  usuwa element z pozycji 0
 
 
+            // Usuwa wszystkie spełniające warunek zwracany przez funkcje bool (testowany jest  na każdm elemencie)
+            bool IsGreaterThan5(int i)
+            {
+                return i > 5;
+            }
+
+            intList.RemoveAll(IsGreaterThan5);
+
+            intList.Remove(1);  // usuwa jedną konkretna wartosc. lista {1,2,1}   po zastosowanie funkcji => {2,1}
+
+            intList.RemoveRange(1, 2);   // ( numer indexu, ilosc elementow do usuniecia)
+
+            intList.Sort(); // sortuje liste
+
+            DispayElements(intList);
 
         }
     }
