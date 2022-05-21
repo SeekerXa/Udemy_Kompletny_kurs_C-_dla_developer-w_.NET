4c8812a0571e30d07d0db39565df8b27df3c24f5
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-05-21 23:31:20 +0200

    Parsing string task

---
+++ ParsingNegative/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingNegative
+{
+    internal class Program
+    {
+        public static int TryNegativeParsing(string input,out int result)
+        {
+            if (int.TryParse(input, out result))
+            {
+
+            }
+            else
+            {
+
+            }
+
+
+        }
+
+        public static bool IsNegative(int x) => x < 0;
+    
+
+
+
+
+        static void Main(string[] args)
+        {
+
+
+            TryNegativeParsing("-22");
+        }
+    }                                                                
+}
