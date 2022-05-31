b0156eec0e248e77692998091839e9be1b269384
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-05-31 14:22:29 +0200

    Using need a IDisposable interface
    "Using" can be used for any class that implements the "IDisposable" interface

--- TryCatch_Using/Program.cs
+++ TryCatch_Using/Program.cs
@@ -14,30 +14,45 @@ namespace TryCatch_Using
             var filePath = @"E:\Projects\Learning\C#\Udemy_Kompletny_kurs_C-_dla_developer-w_.NET\TryCatch_Using\file.txt";
             var fileContent = File.ReadAllText(filePath);
 
-            //var readFileStream = new FileStream(filePath, FileMode.Open);
-            //try
-            //{
+            var readFileStream = new FileStream(filePath, FileMode.Open);
+            try
+            {
 
-            //}
-            //finally
-            //{
-            //    readFileStream.Close();
-            //}
+            }
+            finally
+            {
+                readFileStream.Close();
+            }
 
             //==== this same
             UnicodeEncoding uniencoding = new UnicodeEncoding();
             string text = "Something!";
             byte[] temp = uniencoding.GetBytes(text);
 
-        
+            //"Using" can be used for any class that implements the "IDisposable" interface !!!!!! 
 
-            using (var writeFileStream = new FileStream(filePath, FileMode.Open))
+
+            //That u can do by  "using"
+            var writeFileStream = new FileStream(filePath, FileMode.Open))
+            try
             {
-               // writeFileStream.Write(temp, 0, temp.Length);
-               writeFileStream.Write(temp, 0, temp.Length);
+                // writeFileStream.Write(temp, 0, temp.Length);
+                writeFileStream.Write(temp, 0, temp.Length);
+
+            }
+             finally
+            {
+                ((IDisposable)writeFileStream).Dispose();   
             }
 
-            using (var readFileStream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite))
+
+            using (writeFileStream = new FileStream(filePath, FileMode.Open))
+            {
+                // writeFileStream.Write(temp, 0, temp.Length);
+                writeFileStream.Write(temp, 0, temp.Length);
+            }
+
+            using (readFileStream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite))
             {
                 byte[] b = new byte[1024];
                 UnicodeEncoding tempRead = new UnicodeEncoding();
