fc14ec1e6d0cb836f286b514c0050b7d02124c84
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-06-05 16:51:59 +0200

    DateTime Methods

---
+++ DataTimeMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+;
+Console.WriteLine("Hello, World!");
+DataTimeModification();
+
+
+
+
+
+static void DataTimeModification()
+{
+    DateTime now = DateTime.Now;
+    DateTime openDate = new DateTime(1992, 6, 17);
+    TimeSpan result = now- openDate ;
+
+    Console.WriteLine(result.Days);
+
+
+
+
+}
