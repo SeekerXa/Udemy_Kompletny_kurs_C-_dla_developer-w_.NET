0f339eb5500f646b5363ae2b871d9601b01a4c6b
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-06-05 17:08:38 +0200

    Update Program.cs

--- DataTimeMethods/Program.cs
+++ DataTimeMethods/Program.cs
@@ -12,11 +12,10 @@ static void DataTimeModification()
 {
     DateTime now = DateTime.Now;
     DateTime openDate = new DateTime(1992, 6, 17);
-    TimeSpan result = now- openDate ;
+    TimeSpan result = now - openDate;
 
     Console.WriteLine(result.Days);
 
-
-
-
+    DateTime expiresAt = now.AddDays(7);
+    DateTime expiresAt2 = now.Add(new TimeSpan(4, 3, 3));
 }
