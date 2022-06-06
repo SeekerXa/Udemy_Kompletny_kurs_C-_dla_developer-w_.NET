e1fdb6cf72ea9a2734fd5facca5127e2e836012a
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-06-06 15:16:47 +0200

    DataTimeMethods

--- DataTimeMethods/Program.cs
+++ DataTimeMethods/Program.cs
@@ -2,8 +2,8 @@
 using System;
 ;
 Console.WriteLine("Hello, World!");
-DataTimeModification();
-
+//DataTimeModification();
+DateTimeFormatting();
 
 
 
@@ -18,4 +18,21 @@ static void DataTimeModification()
 
     DateTime expiresAt = now.AddDays(7);
     DateTime expiresAt2 = now.Add(new TimeSpan(4, 3, 3));
+
+    Console.WriteLine(expiresAt);
+    Console.WriteLine(expiresAt2);
+
+    bool expiresAtTheSameDay = expiresAt.Date == expiresAt2.Date;
+    Console.WriteLine($"expires at the same date: {expiresAtTheSameDay}");
+}
+
+static void DateTimeFormatting()
+{
+    DateTime now = DateTime.Now;
+    Console.WriteLine(now.ToShortDateString());
+    Console.WriteLine(now.ToLongDateString());
+    Console.WriteLine(now.ToString("g"));
+    Console.WriteLine(now.ToString("G"));
+    Console.WriteLine(now.ToString("yyyy-MM-dd hh:mm:ss"));  //
+
 }
