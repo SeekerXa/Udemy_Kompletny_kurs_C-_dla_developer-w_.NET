b465a40b14f4c97eed948b1ca4ad6f3f3e4eb998
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-06-08 14:58:27 +0200

    ExtensionMethods
    public static class => public static method with "this" before first value.

---
+++ ExtensionMeth/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ExtensionMeth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            DateTime before = now.Subtract(new TimeSpan(7, 0, 0, 0));
+            DateTime after = now.AddDays(7);
+            Utils.IsDateBetwen(now, before, after);
+
+            now.IsBetwen(before, after);    
+        }
+    }
+}
---
+++ ExtensionMeth/Utils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMeth
+{
+    public static class Utils
+    {
+
+        public static bool IsDateBetwen(DateTime date, DateTime from, DateTime to)
+        {
+            return date > from && date < to;
+
+        }
+        public static bool IsBetwen(this DateTime date, DateTime from, DateTime to)
+        {
+            return date > from && date < to;
+
+        }
+
+        public static int Squared(this int value)
+        {
+            return value * value;
+        }
+    }
+}
