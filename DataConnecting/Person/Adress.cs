f601eeeb0875f4552964c342820ede6a007fe02a
SeekerXa <88826284+SeekerXa@users.noreply.github.com>
2022-08-01 17:36:54 +0200

    DataConnection

---
+++ DataConnecting/Person/Adress.cs
@@ -0,0 +1,12 @@
+﻿
+namespace DataConnecting
+{
+    public class Adress
+    {
+        public int Id { get; set; }
+        public string PersonId { get; set; }
+        public string Streat { get; set; }
+        public string City { get; set; }
+
+    }
+}
---
+++ DataConnecting/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataConnecting;
+using Newtonsoft.Json;
+
+
+var people = LoadPeople();
+var adresses = LoadAdresses();
+
+
+
+
+static List<Person> LoadPeople()
+{
+    var currentPath = Directory.GetParent((Directory.GetCurrentDirectory()));
+    var jsonFullPath = Path.GetRelativePath(currentPath, "DataConnecting/Person/People.json)");
+
+    var json = File.ReadAllText(jsonFullPath);
+    return JsonConvert.DeserializeObject<List<Person>>(json);
+}
+
+
+static List<Adress> LoadAdresses()
+{
+    var currentPath = Directory.GetCurrentDirectory();
+    
+    var jsonFullPath = Path.GetRelativePath(currentPath, "Person/Adresses.json");
+
+    var json = File.ReadAllText(jsonFullPath);
+    return JsonConvert.DeserializeObject<List<Adress>>(json);
+}
+
